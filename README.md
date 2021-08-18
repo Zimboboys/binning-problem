142c5a571456c5d90a179ba0c69fd7f1d6c251e4
Ethan Zimbelman <itszimbo@gmail.com>
2021-08-17 23:45:49 -0700

    generate testing data

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 a quick little problem of finding the closest pairings between all elements of two sets. decided to go way overboard on the math lingo on my brainstorming cause it's "fun"
 
 problem was gifted to me by Kai. thanks Kai 🙏
+
+## getting started
+
+run `python3 sets/generate_sets.py` for testing data, then start hacking away
---
+++ sets/generate_sets.py
@@ -0,0 +1,20 @@
+import random
+
+A = open("./sets/A.csv", "w")
+B = open("./sets/B.csv", "w")
+
+A_count = 200
+B_count = 3000 # actual problem has this at 300 000
+
+# elements of A are of the form (x, y, z, t)
+for i in range(A_count):
+    x, y, z, t = random.random(), random.random(), random.random(), random.random()
+    A.write("{0}, {1}, {2}, {3}\n".format(x, y, z, t))
+
+# elements of B are of the form (x, y, z, t)
+for i in range(B_count):
+    x, y, z = random.random(), random.random(), random.random()
+    A.write("{0}, {1}, {2}\n".format(x, y, z))
+
+A.close()
+B.close()
