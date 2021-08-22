f2fde46a86473f7ac6f633daf306e2f06772f09e
Ethan Zimbelman <itszimbo@gmail.com>
2021-08-22 00:13:12 -0700

    generalize distance calculation

--- point.py
+++ point.py
@@ -5,12 +5,14 @@ class Point:
     def get_point(self):
         return self.point
 
+    # calculates Euclidean distance
     def calc_distance(self, point_2):
-        euclidean = 0
-        for i in range(3): # hard-coded dimension here..
-            delta = point_2.point[i] - self.point[i]
-            euclidean = delta ** 2
-        return euclidean ** 0.5
+        distance = 0
+        dimensions = len(self.point)
+        for i in range(dimensions):
+            diff = point_2.point[i] - self.point[i]
+            distance = diff ** 2
+        return distance ** 0.5
     
     def __str__(self):
         return "({0}, {1}, {2})".format(self.point[0], self.point[1], self.point[2])
