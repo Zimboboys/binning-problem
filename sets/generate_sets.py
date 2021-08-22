b99b43bb0f88594815802892df22fa71b94371a6
Ethan Zimbelman <itszimbo@gmail.com>
2021-08-22 00:14:45 -0700

    expected results script added

--- sets/generate_sets.py
+++ sets/generate_sets.py
@@ -1,20 +1,41 @@
-import random
+import random, os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from point import Point, WeightedPoint
 
 A = open("./sets/A.csv", "w")
 B = open("./sets/B.csv", "w")
+expected = open("./sets/expected.out", "w")
 
-A_count = 200
-B_count = 3000 # actual problem has this at 300000
+A_count = 100 # actual problem has this at 300
+B_count = 20000 # actual problem has this at 300000
 
-# elements of A are of the form (x, y, z, t)
+A_set = []
+B_set = []
+
 for i in range(A_count):
     x, y, z, t = random.random(), random.random(), random.random(), random.random()
-    A.write("{0},{1},{2},{3}\n".format(x, y, z, t))
+    A.write("{},{},{},{}\n".format(x, y, z, t))
+    a_point = Point(x, y, z)
+    a = WeightedPoint(a_point, t)
+    A_set.append(a)
 
-# elements of B are of the form (x, y, z, t)
+# elements of B are Points
 for i in range(B_count):
     x, y, z = random.random(), random.random(), random.random()
-    B.write("{0},{1},{2}\n".format(x, y, z))
+    B.write("{},{},{}\n".format(x, y, z))
+    b = Point(x, y, z)
+    B_set.append(b)
+
+# generate expected pairings
+for b in B_set:
+    min_a, min_dist = None, 2
+    for a in A_set:
+        distance = b.calc_distance(a.get_point())
+        if distance < min_dist:
+            min_a, min_dist = a, distance
+    
+    expected.write("{},{},{},{}\n".format(b.get_point()[0], b.get_point()[1], b.get_point()[2], min_a.get_weight()))
 
 A.close()
 B.close()
+expected.close()
